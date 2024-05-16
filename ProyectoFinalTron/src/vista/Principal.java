1241ba2da94c115dba4cad677b471045c94a0d24
JesusLares23 <jesus.lares233383@potros.itson.edu.mx>
2024-05-15 20:32:10 -0700

    Creación de algunas clases del juego

--- ProyectoFinalTron/src/vista/Principal.java
+++ ProyectoFinalTron/src/vista/Principal.java
@@ -2,6 +2,7 @@
 package vista;
 
 import javax.swing.JFrame;
+import java.util.logging.*;
 
 /**
  *
@@ -14,6 +15,22 @@ public class Principal {
      */
     public static void main(String[] args) {
         JFrame ventana = new JFrame("Juego");
+        Juego juego = new Juego();
+        ventana.add(juego);
+        ventana.setSize(600, 400);
+        ventana.setLocation(70, 200);
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        while (true) {
+            juego.repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ie) {
+                Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, 
+                        null, ie);
+            }
+        }
     }
     
 }
---
+++ ProyectoFinalTron/src/vista/SerpienteJ1.java
@@ -0,0 +1,16 @@
+
+package vista;
+
+/**
+ *
+ * @author darkm
+ */
+public class SerpienteJ1 extends Thread {
+    
+    Juego juego;
+    
+    public SerpienteJ1(Juego juego) {
+        this.juego = juego;
+    }
+    
+}
